ae60395f2a97ee696b7a5564d4bd0fbec98de2ad
huynhbaokhang <73773630+kagnag@users.noreply.github.com>
2022-04-07 21:19:25 +0700

    Khoi tao du an

--- contactbook-frontend/src/main.js
+++ contactbook-frontend/src/main.js
@@ -1,8 +1,4 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import router from "./router";
-createApp(App).use(router).mount("#app");
+import { createApp } from 'vue'
+import App from './App.vue'
 
-createApp(App).mount("#app");
+createApp(App).mount('#app')
